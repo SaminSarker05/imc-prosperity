71d99300b59d45e2c37149b7ee108a8b850f90a5
Samin Sarker <saminsarker05@gmail.com>
2025-04-13 02:01:13 -0400

    adding datamodel classes for all logic

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from datamodel import OrderDepth, TradingState, Order
+from typing import *
+import string
+
 
 """
-    @ Trader class with single method run, including all logic
-    @ run takes input [TraderState]
-    @ output is dictionary called [result] with orders
+    @ Trader class with single method run, includes all order logic
+    @ run method: input [TradingState], output dictionary [result] Dict[str: Trade]
 """
+class Trader:
 
+    def run(self, state: TradingState):
+        result = {}
+
+        return result
+
+
+if __name__ == "__main__":
+    print("hello world")
